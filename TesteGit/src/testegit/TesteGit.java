9050cdd04904b5c9f003f4b4e466232c2f620fff
marcati1 <marcati1@hotmail.com>
2015-12-19 13:31:28 -0200

    Commit sitio33

--- TesteGit/src/testegit/TesteGit.java
+++ TesteGit/src/testegit/TesteGit.java
@@ -17,9 +17,7 @@ public class TesteGit {
     public static void main(String[] args) {
         System.out.println("Alteração primeira linha");
         System.out.println("Linha 2");
-        System.out.println("Linha 3");
-        System.out.println("Linha 4");
-        System.out.println("Alterei no sitioooo");
+        System.out.println("Alterei no sitio 33");
     }
 
 }
