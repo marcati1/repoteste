4d2c420222afc1a888f25a8f317a331299639133
marcati1 <marcati1@hotmail.com>
2015-12-19 12:11:27 -0200

    Commit agora

--- TesteGit/src/testegit/TesteGit.java
+++ TesteGit/src/testegit/TesteGit.java
@@ -17,8 +17,10 @@ public class TesteGit {
     public static void main(String[] args) {
         System.out.println("Alteração primeira linha");
         System.out.println("Linha 2");
-         System.out.println("Linha 3");
-           System.out.println("Linha 4");
+        System.out.println("Linha 3");
+        System.out.println("Linha 4");
+        System.out.println("Linha 5");
+        System.out.println("Linha 6");
     }
-    
+
 }
